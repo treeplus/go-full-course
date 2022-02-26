d902f22e7fecaa046e82c639abd796c72eacbab3
treeplus <>
2022-02-26 11:59:55 +0800

    Printf

--- booking-app/main.go
+++ booking-app/main.go
@@ -7,7 +7,11 @@ func main() {
 	const conferenceTickets = 50
 	var remainingTickets = 50
 
-	fmt.Println("Welcome to", conferenceName, "booking application")
-	fmt.Println("We have total of", conferenceTickets, "tickets and", remainingTickets, "are sill available.")
+	// fmt.Println("Welcome to", conferenceName, "booking application")
+	// fmt.Println("We have total of", conferenceTickets, "tickets and", remainingTickets, "are sill available.")
+	// fmt.Println("Get your tickets here to attend.")
+
+	fmt.Printf("Welcome to %v booking application.\n", conferenceName)
+	fmt.Printf("We have total of %v tickets and %v are sill available.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend.")
 }
