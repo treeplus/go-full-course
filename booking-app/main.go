a3dadbeeb26e0de38309c44ce483d66a42204269
treeplus <>
2022-02-26 12:26:00 +0800

    data types

--- booking-app/main.go
+++ booking-app/main.go
@@ -14,4 +14,11 @@ func main() {
 	fmt.Printf("Welcome to %v booking application.\n", conferenceName)
 	fmt.Printf("We have total of %v tickets and %v are sill available.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend.")
+
+	var userName string
+	var userTickets int
+	userName = "Tom"
+	userTickets = 2
+	fmt.Printf("User %v booked %v tickets.\n", userName, userTickets)
+
 }
