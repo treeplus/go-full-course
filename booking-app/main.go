25d0d1b5995bf0e9123f9650a9d4b3ea8c045c4f
treeplus <>
2022-02-26 11:54:30 +0800

    constant

--- booking-app/main.go
+++ booking-app/main.go
@@ -4,5 +4,10 @@ import "fmt"
 
 func main() {
 	var conferenceName = "Go Conference"
+	const conferenceTickets = 50
+	var remainingTickets = 50
+
 	fmt.Println("Welcome to", conferenceName, "booking application")
+	fmt.Println("We have total of", conferenceTickets, "tickets and", remainingTickets, "are sill available.")
+	fmt.Println("Get your tickets here to attend.")
 }
