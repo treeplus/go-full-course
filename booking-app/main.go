1fe47f2ed00595bb2504c401b0830b959b4b738d
treeplus <>
2022-02-26 12:53:39 +0800

    get input

--- booking-app/main.go
+++ booking-app/main.go
@@ -3,9 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	var conferenceName = "Go Conference"
-	const conferenceTickets = 50
-	var remainingTickets = 50
+	var conferenceName string = "Go Conference"
+	const conferenceTickets int = 50
+	var remainingTickets int = 50
 
 	// fmt.Println("Welcome to", conferenceName, "booking application")
 	// fmt.Println("We have total of", conferenceTickets, "tickets and", remainingTickets, "are sill available.")
@@ -15,10 +15,30 @@ func main() {
 	fmt.Printf("We have total of %v tickets and %v are sill available.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend.")
 
-	var userName string
+	// var userName string
+	// var userTickets int
+	// userName = "Tom"
+	// userTickets = 2
+	// fmt.Printf("User %v booked %v tickets.\n", userName, userTickets)
+
+	// get input
+	var firstName string
+	var lastName string
+	var email string
 	var userTickets int
-	userName = "Tom"
-	userTickets = 2
-	fmt.Printf("User %v booked %v tickets.\n", userName, userTickets)
+
+	fmt.Println("Enter your fist name: ")
+	fmt.Scan(&firstName)
+
+	fmt.Println("Enter your last name: ")
+	fmt.Scan(&lastName)
+
+	fmt.Println("Enter your email address: ")
+	fmt.Scan(&email)
+
+	fmt.Println("Enter your tickets: ")
+	fmt.Scan(&userTickets)
+
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
 
 }
